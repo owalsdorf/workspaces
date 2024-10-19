65b8a5193c3cc8202030d33cc33384daf4ddc5b3
Oscar Walsdorf <o.walsdorf@stpauls.school.nz>
2024-10-19 09:58:13 +0000

    Added column filtering.

--- maybe/application.py
+++ maybe/application.py
@@ -37,7 +37,7 @@ def base_table():
 
     return all_items
 
-def get_items(searchinput, sortvar, sortcolumn):
+def get_items(columns, searchinput, sortvar, sortcolumn):
     # Establish connection to the database and grab the cursor
     conn = get_db_connection()
     cur = conn.cursor()
@@ -47,9 +47,10 @@ def get_items(searchinput, sortvar, sortcolumn):
         sortvar = "DESC"
     if sortcolumn == None:
         sortcolumn = "sku"
+    sqlcolumns = ", ".join(columns)
     print("[LOG] - Attempting to execute SQL code")
     sql = f"""
-    SELECT * FROM tbl_items 
+    SELECT {sqlcolumns} FROM tbl_items 
     WHERE (id LIKE '%{searchinput}%'
     OR sku LIKE '%{searchinput}%'
     OR name LIKE '%{searchinput}%'
@@ -59,12 +60,13 @@ def get_items(searchinput, sortvar, sortcolumn):
     ORDER BY {sortcolumn} {sortvar}
     """
     # Fetching all content from tbl_items, filters, and filters_names
-    changed_items = cur.execute(sql).fetchall()
+    changed_table = cur.execute(sql).fetchall()
     print(f"[LOG] - Table has been updated. Sort type: {sortvar}, column sorted: {sortcolumn}, search input: {searchinput}.")
+    print(f"[LOG] - Columns selected: {sqlcolumns}.")
     print("---------------------------------------------------------------")
     conn.close()
 
-    return changed_items;
+    return changed_table;
 
 app = Flask(__name__, static_url_path='/assets', static_folder='assets');
 
@@ -73,32 +75,41 @@ app = Flask(__name__, static_url_path='/assets', static_folder='assets');
 def index():
 
     # Setting data to get_items() so that I can specify which table to reference in the base.html
-    sortcolumn = 'sku'
-    sortvar = 'DESC'
-    searchinput = '1'
+    sortcolumn = 'id'
+    sortvar = 'ASC'
+    searchinput = ''
+    columns = ['id', 'sku', 'name', 'cat', 'size', 'price']
     data = base_table()
 
     if request.method == 'POST':
         print("[LOG] - POST request detected")
         action = request.form.get("action")
-       
+
+        if action == 'filtering':
+            print("[LOG] - Receiving post request to filter columns")
+            columns = request.form.getlist("columns")
+            print(f"[LOG] - Columns selected: {columns}")
+            # If there are no selected columns, set columns to an empty list
+            if columns == []:
+                columns = ['id', 'sku', 'name', 'cat', 'size', 'price']
+            print(f"[LOG] - Filtering data received. Columns to filter: {columns}")
+            data = get_items(columns, searchinput, sortvar, sortcolumn)
+
         if action == 'sorting':
             print("[LOG] - Receiving post request to sort table")
-            searchinput = request.form.get("search")
             sortvar = request.form.get("sortMethod")
             sortcolumn = request.form.get("sortColumn")
-            data = get_items(searchinput, sortvar, sortcolumn)
+            print(f"[LOG] - Sorting data received. Sorting method: {sortvar}. Column to be sorted: {sortcolumn}")
+            data = get_items(columns, searchinput, sortvar, sortcolumn)
 
         if action == 'searching':
             print("[LOG] - Receiving post request to search table")
             searchinput = request.form.get("search")
-            print(f"[LOG] - Search input: {searchinput}")
-            sortvar = request.form.get("sortMethod")
-            sortcolumn = request.form.get("sortColumn")
-            data = get_items(searchinput, sortvar, sortcolumn)
+            print(f"[LOG] - Searching data received. Search input: {searchinput}.")
+            data = get_items(columns, searchinput, sortvar, sortcolumn)
 
     # Processes base.html file using Jinja, and tables can be accessed using 'items', 'filters', and 'names'.
-    return render_template("website.html", items=data, sortvar=sortvar, sortcolumn=sortcolumn, searchinput=searchinput);
+    return render_template("website.html", items=data, columns=columns);
 
 
 if __name__ == "__main__":
